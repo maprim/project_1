66b26158ca93b2bb70f89c0156c9314aa8ee4156
maprim <msfive7@gmail.com>
2017-09-24 16:25:56 +0400

    check

--- index.js
+++ index.js
@@ -25,3 +25,44 @@ for (var i = 0; i < item.length; i++) {
 })
 
 };		
+
+window.addEventListener('wheel', scroller);
+
+
+window.addEventListener('wheel', scroller);
+
+
+function scroller(test){
+ 
+ test = window.event;
+ let itemActive = document.querySelector('li.active');
+
+ let delta = test.deltaY;
+ let next = itemActive.nextElementSibling;
+ let prev = itemActive.previousElementSibling;
+
+ if(next !== null && delta > 55){//check next elem exist
+    console.log('delta');
+
+    itemActive.classList.remove('active');
+    itemActive.nextElementSibling.classList.add('active');
+    let activeData = document.querySelector('li.active').dataset;
+    let showDiv = activeData.index;
+    document.querySelector('.show').classList.remove('show');
+    document.getElementById(showDiv).classList.add('show');
+
+
+ } 
+ else if (delta < 55 && prev !== null){
+
+    itemActive.classList.remove('active');
+    itemActive.previousElementSibling.classList.add('active');
+    let activeData = document.querySelector('li.active').dataset;
+    let showDiv = activeData.index;
+    document.querySelector('.show').classList.remove('show');
+    document.getElementById(showDiv).classList.add('show');
+
+ }
+
+}
+    
